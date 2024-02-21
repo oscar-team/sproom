62505fb172c175b93e556ce0e3ff97009fe8e6fb
Adnan Shoukat <adnansunny821@gmail.com>
2024-02-21 12:51:10 +0100

    add call method in BaseResource Class

--- src/Resources/BaseResource.php
+++ src/Resources/BaseResource.php
@@ -3,10 +3,17 @@
 namespace OscarTeam\Sproom\Resources;
 
 use Saloon\Http\Connector;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
 
 class BaseResource
 {
     public function __construct(protected Connector $connector)
     {
+    }
+
+    public function call(Request $request): Response
+    {
+        return $this->connector->send($request);
     }
 }
